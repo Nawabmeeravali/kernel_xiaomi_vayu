f906c5eefe347f448ea83c145c1f7138766dca74
Jebaitedneko <Jebaitedneko@gmail.com>
2021-05-12 22:20:50 +0530

    fs: pstore: add minimal changes to properly dump to pstore
    forward ported from 4.9 https://github.com/Jebaitedneko/android_kernel_msm89xx/commit/619300b0c6d66ec15db091653fb626a20d8cfa97\#diff-7dd21e7182c99ad25715b1f3741e77ba8f3843f1f9207fa3e7c3b711b4d6ef5a

--- fs/pstore/ram.c
+++ fs/pstore/ram.c
@@ -38,31 +38,31 @@
 #include <linux/memblock.h>
 
 #define RAMOOPS_KERNMSG_HDR "===="
-#define MIN_MEM_SIZE 4096UL
+#define MIN_MEM_SIZE 4096UL // 16KB
 
-static ulong record_size = MIN_MEM_SIZE;
+static ulong record_size = 256 * MIN_MEM_SIZE; // 4MB
 module_param(record_size, ulong, 0400);
 MODULE_PARM_DESC(record_size,
 		"size of each dump done on oops/panic");
 
-static ulong ramoops_console_size = MIN_MEM_SIZE;
+static ulong ramoops_console_size = 256 * MIN_MEM_SIZE; // 4MB
 module_param_named(console_size, ramoops_console_size, ulong, 0400);
 MODULE_PARM_DESC(console_size, "size of kernel console log");
 
-static ulong ramoops_ftrace_size = MIN_MEM_SIZE;
+static ulong ramoops_ftrace_size = 256 * MIN_MEM_SIZE; // 4MB
 module_param_named(ftrace_size, ramoops_ftrace_size, ulong, 0400);
 MODULE_PARM_DESC(ftrace_size, "size of ftrace log");
 
-static ulong ramoops_pmsg_size = MIN_MEM_SIZE;
+static ulong ramoops_pmsg_size = 256 * MIN_MEM_SIZE; // 4MB
 module_param_named(pmsg_size, ramoops_pmsg_size, ulong, 0400);
 MODULE_PARM_DESC(pmsg_size, "size of user space message log");
 
-static unsigned long long mem_address;
+static unsigned long long mem_address = 0xaf000000;
 module_param_hw(mem_address, ullong, other, 0400);
 MODULE_PARM_DESC(mem_address,
 		"start of reserved RAM used to store oops/panic logs");
 
-static ulong mem_size;
+static ulong mem_size = 0x1000000; // 1024 * MIN_MEM_SIZE or 16MB
 module_param(mem_size, ulong, 0400);
 MODULE_PARM_DESC(mem_size,
 		"size of reserved RAM used to store oops/panic logs");
